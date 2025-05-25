30a0c4b48b293b5920af36bce194d1cf85b3b0d1
eitsupi <50911393+eitsupi@users.noreply.github.com>
2025-05-25 09:18:37 +0000

    build: fix command

--- generate.sh
+++ generate.sh
@@ -5,4 +5,4 @@ do
 	curl https://raw.githubusercontent.com/apache/arrow/master/format/$NAME.fbs -o $NAME.fbs
 done
 
-planus rust *.fbs -o src/ipc/generated.rs
+planus --ignore-docstring-errors rust *.fbs -o src/ipc/generated.rs
